8b759222672cfd2c0e0bad5556cc72a3ed791071
Harshad Kakatkar <harshad.kakatkar@student.shu.ac.uk>
2025-03-16 13:07:05 +0000

    week4

---
+++ week4/action_page.php
@@ -0,0 +1,14 @@
+<html>
+<body>
+
+Submitted Form Data
+Your input was received as:
+
+
+First Name: <?php echo $_POST["fname"]; ?><br>
+Last Name: <?php echo $_POST["lname"]; ?><br>
+
+
+
+</body>
+</html>
